9ec8c76bdb33eb68333b65fdd2f5260a67c0364f
Scott Ogden <sogden@izeni.com>
2016-01-07 09:04:47 -0700

    Server now directs to index.html when accessed.

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 var express = require('express'),
-    app = express();
+    app = express(),
+    path = require('path');
 
-app.get('/', () => {});
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/Public/index.html'));
+});
 
 app.listen(3000);
 console.log("Server running on port 3000");
