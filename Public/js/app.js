850ebe1d535c8de187daf818c8b4a7364e2824ff
gabemeola <gabemeola@gmail.com>
2016-01-17 19:02:08 -0700

    fixed layout issues and naming conventions errors

--- Public/js/app.js
+++ Public/js/app.js
@@ -1,7 +1,8 @@
 angular.module('testerApp', ['ui.router','ui.grid', 'ngAnimate', 'ngAria', 'ngMaterial'])
+
 .config(['$urlRouterProvider', '$stateProvider', '$mdThemingProvider', function ($urlRouterProvider, $stateProvider, $mdThemingProvider) {
 	$urlRouterProvider.otherwise('/home');
-    $stateProvider
+	$stateProvider
 	.state('home', {
 		url: '/home',
 		templateUrl: 'templates/homeTmpl.html',
@@ -15,42 +16,48 @@ angular.module('testerApp', ['ui.router','ui.grid', 'ngAnimate', 'ngAria', 'ngMa
 	.state('teacherRequest', {
 		url: '/teacherRequest',
 		templateUrl: 'templates/teacherRequestTmpl.html',
-		controller: 'teacherRequestCtrl'
+		controller: 'teacherRequestCtl'
 	})
-    .state('studentInfo', {
-        url: '/studentInfo',
-        templateUrl: 'templates/studentInfoTmpl.html',
-        controller: 'studentInfoCtrl'
-    })
-        .state('classInfo', {
-        url: '/classInfo',
-        templateUrl: 'templates/classInfoTmpl.html',
-        controller: 'classInfoCtrl'
-    });
-    $mdThemingProvider.theme('default')
-	 .dark()
-	.accentPalette('blue', {
-		'default': '500',
-		'hue-1': '300',
-		'hue-1': '200',
-		'hue-1': '50',
-
+	.state('studentLogin', {
+		url: '/studentLogin',
+		templateUrl: 'templates/studentLoginTmpl.html',
+		controller: 'studentLoginCtrl'
 	})
-	.primaryPalette('green', {
-		'default': '500',
-		'hue-1': '300',
-		'hue-1': '200',
-		'hue-1': '50',
+	.state('studentInfo', {
+		url: '/studentInfo',
+		templateUrl: 'templates/studentInfoTmpl.html',
+		controller: 'studentInfoCtrl'
 	})
-	.warnPalette('red', {
-		'default': '500',
-		'hue-1': '300',
-		'hue-1': '200',
-		'hue-1': '50',
+	.state('classInfo', {
+		url: '/classInfo',
+		templateUrl: 'templates/classInfoTmpl.html',
+		controller: 'classInfoCtrl'
 	});
-    // .state('student', {
-	//  	url: '/student/:studentId',
-	//  	templateUrl: 'templates/studentTmpl.html',
-	//  	controller: 'studentCtrl'
-    // });
+//		.state('student', {
+//				url: '/student/:studentId',
+//				templateUrl: 'templates/studentTmpl.html',
+//				controller: 'studentCtrl'
+//     });
+	
+	
+	$mdThemingProvider.theme('default')
+		.dark()
+		.accentPalette('blue', {
+			'default': '500',
+			'hue-1': '300',
+			'hue-1': '200',
+			'hue-1': '50',
+		})
+		.primaryPalette('green', {
+			'default': '500',
+			'hue-1': '300',
+			'hue-1': '200',
+			'hue-1': '50',
+		})
+		.warnPalette('red', {
+			'default': '500',
+			'hue-1': '300',
+			'hue-1': '200',
+			'hue-1': '50',
+		});
 }]);
