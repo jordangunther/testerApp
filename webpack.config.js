106e7528ff6f66b44850ac1379a4b640c6563748
gabemeola <gabemeola@gmail.com>
2016-04-03 13:01:17 -0600

    set new home template

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ module.exports = {
 				test: /\.(jpe?g|png|gif|svg)$/i,
 				loaders: [
 					'file?hash=sha512&digest=hex&name=./assets/min-icons/[hash].[ext]',
-					'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false&progressive=true'
+					'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=true&progressive=true'
 				]
 			}
 		]
