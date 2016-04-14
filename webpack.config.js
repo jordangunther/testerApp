df61788cc2a4863e90f5768df70c39174c1d88a0
gabemeola <gabemeola@gmail.com>
2016-04-13 19:29:14 -0600

    remove svg from font loader

--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,7 @@ module.exports = {
 	      loader: ExtractTextPlugin.extract('css-loader?sourceMap!postcss-loader!resolve-url!sass-loader?indentedSyntax')
       },
 			{ //Loads the font files from imports
-				test:  /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+				test:  /\.(ttf|eot|woff(2)?)(\?[a-z0-9]+)?$/,
 				loader: 'file-loader?name=./assets/fonts/[name].[ext]&context=./assets'
 			},
 			{ //Optimizes Images
