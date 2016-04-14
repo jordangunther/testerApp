e059c9cb597c33afc8044c137b6ed8dd8946fa69
gabemeola <gabemeola@gmail.com>
2016-04-14 10:28:35 -0600

    in progress

--- Public/js/controllers/courseSelectCtrl.js
+++
@@ -1,5 +0,0 @@
-angular.module('testerApp')
-.controller('courseSelectCtrl', function($scope, courseSelectService){
-	$scope.courses = [{name: "course1"}, {name: "course2"}];
-	$scope.courseSelected = "course1";
-})
--- Public/js/controllers/testQuestionsCtrl.js
+++
@@ -1,6 +0,0 @@
-angular.module('testerApp')
-.controller('testQuestionsCtrl', function($scope) {
-	$scope.data = {
-  		selected : '',
-	};
-});
