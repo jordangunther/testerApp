8a54d1cd382cdb34544acd3774cd4b4e4f8163c9
jordangunther <jordankgunther@gmail.com>
2016-04-21 07:55:12 -0600

    still working on studentcourse template

--- public/components/student-landing/course-select/courseSelect.ctrl.js
+++ public/components/student-landing/course-select/courseSelect.ctrl.js
@@ -2,5 +2,6 @@ angular.module('testerApp')
 .controller('courseSelectCtrl', function($scope){
 	$scope.title = "List of Courses";
 	// $scope.courses = [{name: "course1", discription: "this is about math", score: null}, {name: "course2", discription: "this is about science", score: null}, {name: "course1", discription: "this is about math", score: 80%}]
-	$scope.courses = ["course1", "course2", "course3"];
+	$scope.courses = ["course1", "course2", "course3","course4", "course5", "course6","course7", "course8", "course9"];
+
 })
